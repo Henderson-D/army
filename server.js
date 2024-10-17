95404e79ebac37917bf5b3e08f9c69d65f0e7c02
-Derrick <2019dwh@gmail.com>
2024-10-16 21:45:56 -0500

    ignore server

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -9,11 +10,17 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static('public')); // Serve static files from the public directory
 
 // Connect to MongoDB
-mongoose.connect('YOUR_MONGODB_CONNECTION_STRING', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.log(err));
+const mongoURI = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.a6dfa.mongodb.net/myDatabase?retryWrites=true&w=majority`;
+
+mongoose.connect(mongoURI, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log('MongoDB connection error:', err));
 
 // Define the schema and model
 const RunningTimeSchema = new mongoose.Schema({
@@ -38,6 +45,11 @@ app.get('/api/running-times', (req, res) => {
     RunningTime.find()
         .then(times => res.json(times))
         .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// Serve the main HTML file when accessing the root URL
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/Index.html'); // Make sure the path is correct
 });
 
 // Start the server
